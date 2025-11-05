c003b1e151de865dde0fc0af8bf9359bf75f2394
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-11-05 15:01:46 +0100

    Add `assembler::reader::Reader` type

---
+++ src/assembler/reader.rs
@@ -0,0 +1,62 @@
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Reader<'a> {
+    buffer: &'a [u8],
+}
+
+impl<'a> Reader<'a> {
+    pub fn new(slice: &[u8]) -> Reader {
+        Reader { buffer: slice }
+    }
+
+    pub fn read(&mut self) -> Option<u8> {
+        self.read_n(1).map(|slice| slice[0])
+    }
+
+    pub fn read_n<'b>(&'b mut self, n: usize) -> Option<&'a [u8]> {
+        if self.buffer.len() < n { return None; }
+        let (ret, rest) = self.buffer.split_at(n);
+        self.buffer = rest;
+        Some(ret)
+    }
+
+    pub fn skip(&mut self) {
+        let _ = self.read();
+    }
+
+    pub fn skip_n(&mut self, n: usize) {
+        let _ = self.read_n(n);
+    }
+
+    pub fn peek(&self) -> Option<u8> {
+        self.peek_n(1).map(|slice| slice[0])
+    }
+
+    pub fn peek_at(&self, index: usize) -> Option<u8> {
+        if self.buffer.len() <= index { return None; }
+        Some(self.buffer[index])
+    }
+
+    pub fn peek_n<'b>(&'b self, n: usize) -> Option<&'a [u8]> {
+        if self.buffer.len() < n { return None; }
+        Some(&self.buffer[..n])
+    }
+
+    pub fn read_until<'b>(&'b mut self, value: u8) -> Option<&'a [u8]> {
+        for i in 0..self.buffer.len() {
+            if self.peek_at(i).unwrap() == value {
+                let ret = self.read_n(i).unwrap();
+                self.skip();
+                return Some(ret);
+            }
+        }
+        None
+    }
+
+    pub fn read_until_or_end<'b>(&'b mut self, value: u8) -> &'a [u8] {
+        self.read_until(value).unwrap_or_else(|| self.read_n(self.buffer.len()).unwrap())
+    }
+
+    pub fn at_end(&self) -> bool {
+        self.buffer.len() == 0
+    }
+}
