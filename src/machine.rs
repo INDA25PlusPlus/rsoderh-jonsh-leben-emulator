82a1fb0fe15f20f071b99252e655777f6faf2dd5
Jonathan Hahne <jonathanhahne@n148-p5.eduroam.kth.se>
2025-11-06 21:27:29 +0100

    feat: Implemented sub instruction and test

--- src/machine.rs
+++ src/machine.rs
@@ -303,6 +303,8 @@ impl Machine {
                 let result = a.wrapping_add(value);
                 if result < a.max(value) {
                     self.conditions.set(ConditionRegister::Carry, true);
+                } else {
+                    self.conditions.set(ConditionRegister::Carry, false);
                 }
                 self.registers
                     .set_8(Register::A(()), result, &mut self.memory);
@@ -326,30 +328,33 @@ impl Machine {
                 if self.conditions.get(ConditionRegister::Carry) {
                     (result, overflow_2) = result.overflowing_add(1);
                 }
-                if !overflow_1 && !overflow_2 {
-                    self.conditions.set(ConditionRegister::Carry, false);
-                } else {
-                    self.conditions.set(ConditionRegister::Carry, true);
-                }
+                self.conditions
+                    .set(ConditionRegister::Carry, overflow_1 || overflow_2);
                 self.registers
                     .set_8(Register::A(()), result, &mut self.memory);
                 ExecutionResult::Running
             }
             Instruction::Aci(data) => {
                 let a = self.registers.get_8(Register::A(()), &self.memory);
-                let (mut result, mut overflow) = a.overflowing_add(data);
+                let (mut result, overflow_1) = a.overflowing_add(data);
+                let mut overflow_2 = false;
                 if self.conditions.get(ConditionRegister::Carry) {
-                    self.conditions.set(ConditionRegister::Carry, false);
-                    (result, overflow) = result.overflowing_add(1);
+                    (result, overflow_2) = result.overflowing_add(1);
                 }
-                if overflow {
-                    self.conditions.set(ConditionRegister::Carry, true);
-                }
+                self.conditions
+                    .set(ConditionRegister::Carry, overflow_1 || overflow_2);
                 self.registers
                     .set_8(Register::A(()), result, &mut self.memory);
                 ExecutionResult::Running
             }
-            Instruction::Sub(_register) => unimplemented!(),
+            Instruction::Sub(register) => {
+                let a = self.registers.get_8(Register::A(()), &self.memory);
+                let value = self.register_8(register);
+                let result = a.wrapping_sub(value);
+                self.registers
+                    .set_8(Register::A(()), result, &mut self.memory);
+                ExecutionResult::Running
+            }
             Instruction::Sui(_) => unimplemented!(),
             Instruction::Sbb(_register) => unimplemented!(),
             Instruction::Sbi(_) => unimplemented!(),
@@ -398,9 +403,11 @@ impl Machine {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Instant;
 
     #[test]
     fn test_add_register() {
+        let now = Instant::now();
         let mut machine = Machine::new();
 
         machine.conditions.set(ConditionRegister::Carry, true);
@@ -413,9 +420,33 @@ mod tests {
             .set_8(Register::B(()), 0x35, &mut machine.memory);
 
         let result = machine.execute(Instruction::Adc(Register::B(())));
+        let elapsed = now.elapsed();
 
         assert_eq!(result, ExecutionResult::Running);
 
-        assert_eq!(0x6B, machine.register_8(Register::A(())))
+        assert_eq!(0x6B, machine.register_8(Register::A(())));
+        println!("Time elapsed: {:?}", elapsed);
+    }
+    #[test]
+    fn test_sub_register() {
+        let now = Instant::now();
+        let mut machine = Machine::new();
+
+        // machine.conditions.set(ConditionRegister::Carry, true);
+
+        machine
+            .registers
+            .set_8(Register::A(()), 0x20, &mut machine.memory);
+        machine
+            .registers
+            .set_8(Register::B(()), 0x10, &mut machine.memory);
+        let result = machine.execute(Instruction::Sub(Register::B(())));
+        let elapsed = now.elapsed();
+
+        assert_eq!(result, ExecutionResult::Running);
+
+        assert_eq!(0x10, machine.register_8(Register::A(())));
+
+        println!("Time elapsed: {:?}", elapsed);
     }
 }
